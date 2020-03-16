f97ddf18e4bdeb8d634bd76feb8daa32f55138a6
Owan Hunte <owanh@hotmail.com>
2020-03-16 01:29:15 -0400

    Implements app footer.

---
+++ src/components/layouts/v1/AppFooter.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "../../../styles/v1/theme.module.scss";
+import cx from "classnames";
+
+const AppFooter = () => {
+  return (
+    <footer className={styles.appFooter}>
+      <div className={cx(styles.caption, styles.footerDiv1)}>
+        <span>
+          &copy; {new Date().getUTCFullYear()}. This web app was created by{" "}
+          <a
+            href="https://owanhunte.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Owan Hunte
+          </a>
+          , a web developer from <em>Barbados</em>.
+        </span>
+      </div>
+      <div className={cx(styles.caption, styles.footerDiv2)}>
+        Stats, news and other data relevant to the Caribbean region, on the
+        COVID-19 Corona virus.
+      </div>
+    </footer>
+  );
+};
+
+export default AppFooter;
--- src/components/layouts/v1/Layout.tsx
+++ src/components/layouts/v1/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
 import AppHeader from "./AppHeader";
+import AppFooter from "./AppFooter";
 import styles from "../../../styles/v1/theme.module.scss";
 import cx from "classnames";
 
@@ -12,6 +13,7 @@ const Layout: FunctionComponent<Props> = ({ children }) => {
     <div className={cx(styles.appRoot, styles.isFullScreen)}>
       <AppHeader />
       <main className={styles.appMain}>{children}</main>
+      <AppFooter />
     </div>
   );
 };
